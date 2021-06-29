83c8839ca2c7d2a981e5929467f48021c9701a5c
Newsha Hosseiny Nik <nioosha.h@gmail.com>
2021-06-29 16:51:09 +0430

    added proper logic. renamed contact-me.html file. finished project.

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const http = require("http");
 const fs = require("fs");
-const events = require("events");
-const eventEmitter = events.EventEmitter();
 
 http
-  .createServer(function (req, res) {
-    const baseURL = "http://" + req.headers.host + "/";
-    const reqUrl = new URL(req.url, baseURL);
-    var filename = "." + reqUrl.pathname + ".html";
-
-    document.getElementById("about-me").onclick = function changePage() {
-      filename = "./about.html";
-    };
-
-    document.getElementById("contact-me").onclick = function changePage() {
-      filename = "./contact-me.html";
-    };
-
-    document.getElementById("home").onclick = function changePage() {
-      filename = "./index.html";
-    };
-
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      fs.readFile("index.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html;" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === "/about") {
+      fs.readFile("about.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (req.url === "/contact") {
+      fs.readFile("contact.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("404.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    }
   })
   .listen(8080);
